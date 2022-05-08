8b7fc33711599a08669921739d1b3228e7047fff
BarInABox <92856476+BarInABox@users.noreply.github.com>
2022-05-08 17:54:02 +0200

    Update RoboterV2.ino

--- RoboterV2.ino
+++ RoboterV2.ino
@@ -1,10 +1,13 @@
 
 
+#define motorInterfaceType 1
 /*
                INPUTS
       Pos     end    Pos     end
          Base         Shoulder
    I45,44I I43,42I I41,40I I39,38I
+   Not connected    L_Base T_Hand
+   I37,36I I35,34I  I33,32I I31,30I  // Enstops Stepper
        Turn Ell       Buckle Ell
    I53,52I I51,50I I49,48I I47,46I
      Buckle Hand   Close Open Hand
@@ -24,82 +27,82 @@
 
 */
 
-const int Pin_EndStop_L_Base = A6;
-const int PIN_EndStoP2_L_Base = A7;
-const int PIN_OUTPUT_FIRST_L_Base = A1;
-const int PIN_OUTPUT_SECOND_L_Base = A0;
+#define PIN_EndStop_L_Base   33
+#define PIN_EndStop2_L_Base   32
+#define PIN_dir_L_Base   A1
+#define PIN_Step_L_Base   A0
 
-const int PIN_SENSOR_ONEBase = 44;
-const int PIN_SENSOR_TWOBase = 45;
-const int Pin_EndStop_Base = 43;
-const int PIN_EndStoP2_Base = 42;
-const int PIN_OUTPUT_FIRST_Base = 2;
-const int PIN_OUTPUT_SECOND_Base = 3;
+#define PIN_SENSOR_ONEBase   44
+#define PIN_SENSOR_TWOBase   45
+#define Pin_EndStop_Base   43
+#define PIN_EndStoP2_Base   42
+#define PIN_OUTPUT_FIRST_Base   2
+#define PIN_OUTPUT_SECOND_Base   3
 int startPositionOneBase = 0;
 int startPositionTwoBase = 0;
 int sensorPositionOneBase = 0;
 int sensorPositionTwoBase = 0;
 
-const int PIN_SENSOR_ONEShoulder = 41;
-const int PIN_SENSOR_TWOShoulder = 40;
-const int Pin_EndStop_Shoulder = 39;
-const int PIN_EndStoP2_Shoulder = 38;
-const int PIN_OUTPUT_FIRST_Shoulder = 4;
-const int PIN_OUTPUT_SECOND_Shoulder = 5;
+#define PIN_SENSOR_ONEShoulder   41
+#define PIN_SENSOR_TWOShoulder   40
+#define Pin_EndStop_Shoulder   39
+#define PIN_EndStoP2_Shoulder   38
+#define PIN_OUTPUT_FIRST_Shoulder   4
+#define PIN_OUTPUT_SECOND_Shoulder   5
 int startPositionOneShoulder = 0;
 int startPositionTwoShoulder = 0;
 int sensorPositionOneShoulder = 0;
 int sensorPositionTwoShoulder = 0;
 
-const int PIN_SENSOR_ONET_Ellbow = 53;
-const int PIN_SENSOR_TWOT_Ellbow = 52;
-const int Pin_EndStop_T_Ellbow = 51;
-const int PIN_EndStoP2_T_Ellbow = 50;
-const int PIN_OUTPUT_FIRST_T_Ellbow = 6;
-const int PIN_OUTPUT_SECOND_T_Ellbow = 7;
+#define PIN_SENSOR_ONET_Ellbow   53
+#define PIN_SENSOR_TWOT_Ellbow   52
+#define Pin_EndStop_T_Ellbow   51
+#define PIN_EndStoP2_T_Ellbow   50
+#define PIN_OUTPUT_FIRST_T_Ellbow   6
+#define PIN_OUTPUT_SECOND_T_Ellbow   7
 int startPositionOneT_Ellbow = 0;
 int startPositionTwoT_Ellbow = 0;
 int sensorPositionOneT_Ellbow = 0;
 int sensorPositionTwoT_Ellbow = 0;
 
 
-const int PIN_SENSOR_ONEB_Ellbow = 49;
-const int PIN_SENSOR_TWOB_Ellbow = 48;
-const int Pin_EndStop_B_Ellbow = 47;
-const int PIN_EndStoP2_B_Ellbow = 46;
-const int PIN_OUTPUT_FIRST_B_Ellbow = 9;
-const int PIN_OUTPUT_SECOND_B_Ellbow = 8;
+#define PIN_SENSOR_ONEB_Ellbow   49
+#define PIN_SENSOR_TWOB_Ellbow   48
+#define Pin_EndStop_B_Ellbow   47
+#define PIN_EndStoP2_B_Ellbow   46
+#define PIN_OUTPUT_FIRST_B_Ellbow   9
+#define PIN_OUTPUT_SECOND_B_Ellbow   8
 int startPositionOneB_Ellbow = 0;
 int startPositionTwoB_Ellbow = 0;
 int sensorPositionOneB_Ellbow = 0;
 int sensorPositionTwoB_Ellbow = 0;
 
-const int PIN_SENSOR_ONEB_Hand = 28;
-const int PIN_SENSOR_TWOB_Hand = 29;
-const int Pin_EndStop_B_Hand = 27;
-const int PIN_EndStoP2_B_Hand = 26;
-const int PIN_OUTPUT_FIRST_B_Hand = 10;
-const int PIN_OUTPUT_SECOND_B_Hand = 11;
+#define PIN_SENSOR_ONEB_Hand   28
+#define PIN_SENSOR_TWOB_Hand   29
+#define Pin_EndStop_B_Hand   27
+#define PIN_EndStoP2_B_Hand   26
+#define PIN_OUTPUT_FIRST_B_Hand   10
+#define PIN_OUTPUT_SECOND_B_Hand   11
 int startPositionOneB_Hand = 0;
 int startPositionTwoB_Hand = 0;
 int sensorPositionOneB_Hand = 0;
 int sensorPositionTwoB_Hand = 0;
 
-const int PIN_SENSOR_ONEC_Hand = 25;
-const int PIN_SENSOR_TWOC_Hand = 24;
-const int Pin_EndStop_C_Hand = 23;
-const int PIN_EndStoP2_C_Hand = 22;
-const int PIN_OUTPUT_FIRST_C_Hand = 12;
-const int PIN_OUTPUT_SECOND_C_Hand = 13;
+#define PIN_SENSOR_ONEC_Hand   25
+#define PIN_SENSOR_TWOC_Hand   24
+#define Pin_EndStop_C_Hand   23
+#define PIN_EndStoP2_C_Hand   22
+#define PIN_OUTPUT_FIRST_C_Hand   12
+#define PIN_OUTPUT_SECOND_C_Hand   13
 int startPositionOneC_Hand = 0;
 int startPositionTwoC_Hand = 0;
 int sensorPositionOneC_Hand = 0;
 int sensorPositionTwoC_Hand = 0;
 
-const int Pin_EndStop_T_Hand = A6;
-const int PIN_EndStoP2_T_Hand = A7;
-const int PIN_OUTPUT_FIRST_T_Hand = A1;
-const int PIN_OUTPUT_SECOND_T_Hand = A0;
+#define Pin_EndStop_T_Hand   30
+#define PIN_EndStoP2_T_Hand   31
+#define PIN_dir_T_Hand   A1
+#define PIN_Step_T_Hand   A0
 
 
 struct Axis {
@@ -120,16 +123,37 @@ struct Axis {
   int PIN_Sens2;
 
 };
+struct StepperAxis {
+  int StepperSpeed;
+  int PIN_Direction;
+  int PIN_Steps;
+  int Endstop1;
+  int Endstop2;
+  int goal;
+  int Position;
+  int Steps;
 
+};
+typedef struct StepperAxis MyStepperAxis;
 typedef struct Axis MyAxis;
 
-MyAxis L_Base, Base, Shoulder, T_Ellbow, B_Ellbow, T_Hand, B_Hand, C_Hand;
+MyAxis  Base, Shoulder, T_Ellbow, B_Ellbow,  B_Hand, C_Hand;
+MyStepperAxis L_Base, T_Hand;
 
 void setup() {
   // put your setup code here, to run once:
   long SearchEnd = -1000000;
   Serial.begin(9600);
   Serial.println("Started up");
+
+
+  pinMode(PIN_EndStop_L_Base, INPUT_PULLUP);
+  pinMode(PIN_EndStop2_L_Base, INPUT_PULLUP);
+  pinMode(PIN_dir_L_Base, OUTPUT);
+  pinMode(PIN_Step_L_Base, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(PIN_EndStop_L_Base), Endstop_L_B, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_EndStop2_L_Base), Endstop2_L_B, CHANGE);
+  init_StepperAxis(&L_Base, PIN_dir_L_Base, PIN_Step_L_Base, PIN_EndStop_L_Base, PIN_EndStop2_L_Base, SearchEnd, 0);
 
   pinMode(PIN_SENSOR_ONEBase, INPUT_PULLUP);
   pinMode(PIN_SENSOR_TWOBase, INPUT_PULLUP);
@@ -181,6 +205,14 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(PIN_SENSOR_TWOB_Hand), PositionOfB_Hand, CHANGE);
   init_Axis(&B_Hand, 0, 0, PIN_OUTPUT_FIRST_B_Hand, PIN_OUTPUT_SECOND_B_Hand, Pin_EndStop_B_Hand, PIN_EndStoP2_B_Hand, 0, SearchEnd, 0, 0, 0, digitalRead(PIN_SENSOR_ONEB_Hand), digitalRead(PIN_SENSOR_TWOB_Hand), PIN_SENSOR_ONEB_Hand, PIN_SENSOR_TWOB_Hand);
 
+  pinMode(Pin_EndStop_T_Hand, INPUT);
+  pinMode(PIN_EndStoP2_T_Hand, INPUT);
+  pinMode(PIN_dir_T_Hand, OUTPUT);
+  pinMode(PIN_Step_T_Hand, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(Pin_EndStop_T_Hand), Endstop_T_H, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_EndStoP2_T_Hand), Endstop2_T_H, CHANGE);
+  init_StepperAxis(&T_Hand, PIN_dir_T_Hand, PIN_Step_T_Hand, Pin_EndStop_T_Hand, PIN_EndStoP2_T_Hand, SearchEnd, 0);
+
   pinMode(PIN_SENSOR_ONEC_Hand, INPUT_PULLUP);
   pinMode(PIN_SENSOR_TWOC_Hand, INPUT_PULLUP);
   pinMode(Pin_EndStop_C_Hand, INPUT_PULLUP);
@@ -193,6 +225,20 @@ void setup() {
 
 
   // homing();
+}
+void Endstop_L_B() {
+  MyStepperAxis* data = &L_Base;
+  data->Endstop1 = digitalRead(PIN_EndStop_L_Base);
+}
+void Endstop2_L_B() {
+  MyStepperAxis* data = &L_Base;
+  data->Endstop2 = digitalRead(PIN_EndStop2_L_Base);
+}
+void Endstop_T_H() {
+
+}
+void Endstop2_T_H() {
+
 }
 void init_Axis(MyAxis* data, int speed1, int speed2, int output1, int output2, int Endstop1, int Endstop2, long Distance, long goal, long Position,  int StartPositionOne , int StartPositionTwo, int SensorPositionOne, int SensorPositionTwo, int PIN_Sens1,  int PIN_Sens2 ) {
   data->speed1 = speed1;
@@ -211,29 +257,30 @@ void init_Axis(MyAxis* data, int speed1, int speed2, int output1, int output2, i
   data->PIN_Sens1 = PIN_Sens1;
   data->PIN_Sens2 = PIN_Sens2;
 }
+void init_StepperAxis(MyStepperAxis* data, int PIN_Direction , int PIN_Steps, int Endstop1, int Endstop2, int goal, int Position) {
+  data->PIN_Direction = PIN_Direction;
+  data->PIN_Steps = PIN_Steps;
+  data->Endstop1 = Endstop1;
+  data->Endstop2 = Endstop2;
+  data->goal = goal;
+  data->Position = Position;
+}
 
 void PositionOfBase() {
   Position(&Base);
 }
-
-
 void PositionOfShoulder() {
   Position(&Shoulder);
 }
-
 void PositionOfT_Ellbow() {
   Position(&T_Ellbow);
 }
-
-
 void PositionOfB_Ellbow() {
   Position(&B_Ellbow);
 }
-
 void PositionOfB_Hand() {
   Position(&B_Hand);
 }
-
 void PositionOfC_Hand() {
   Position(&C_Hand);
 }
@@ -245,27 +292,20 @@ void Position(MyAxis*data) {
     if (data->SensorPositionTwo == 1) {
       data->Position++;
     } else {
-
       data->Position--;
       //   Serial.println(data->Position);
     }
   } else if (data->SensorPositionTwo == 1) {
     data->Position--;
-
   } else {
     data->Position++;
-
     // Serial.println(data->Position);
-
   }
   data->StartPositionOne = data->SensorPositionOne;
   data->StartPositionTwo = data->SensorPositionTwo;
 }
 
-
-
 int GetSpeed(MyAxis* data) {
-
   if (data->Distance < 50) {
     return 50;
   } else if (data->Distance < 100) {
@@ -274,6 +314,7 @@ int GetSpeed(MyAxis* data) {
     return 255;
   }
 }
+
 void updateDistance(MyAxis* data) {
   if (data->goal < data->Position) {
     data->speed1 = 0;
@@ -287,16 +328,11 @@ void updateDistance(MyAxis* data) {
 }
 
 int moveAxis(MyAxis* data) {
-
   updateDistance(data);
   while (!(data->Distance <= 10) && digitalRead(data->Endstop1) == 1 && digitalRead(data->Endstop2) == 1 ) {
-
     updateDistance(data);
-
-
     analogWrite(data->output1, data->speed1 );
     analogWrite(data->output2, data->speed2 );
-
   }
   analogWrite(data->output1, 0 );
   analogWrite(data->output2, 0 );
@@ -304,10 +340,8 @@ int moveAxis(MyAxis* data) {
   data->speed2 = 0;
   Serial.println("Dis");
   Serial.println(data->Distance);
-
   Serial.println("Pos");
   Serial.println(data->Position);
-
   Serial.println("goal");
   Serial.println(data->goal);
   if (digitalRead(data->Endstop1) == 1 && digitalRead(data->Endstop2) == 0) {
@@ -319,7 +353,41 @@ int moveAxis(MyAxis* data) {
     return 0;
   }
 }
-
+int moveStepperAxis(MyStepperAxis* data) {
+  bool stpState = LOW;
+  unsigned long currentMillis = millis();
+  unsigned long previousMillis;
+  while (data->Endstop2 && data->Endstop1 && data->goal != data->Steps) {
+    if (data->goal > data->Position) {
+      digitalWrite(data->PIN_Direction, HIGH);
+    } else if (data->goal < data->Position) {
+      digitalWrite(data->PIN_Direction, LOW);
+    } else {
+      return 0;
+    }
+    while(data->Steps > 0) {
+      currentMillis = millis();
+      if (currentMillis - previousMillis >= 500) {
+        previousMillis = currentMillis;
+        if (stpState == LOW) {
+          stpState = HIGH;
+          data->Steps--;
+        } else {
+          stpState = LOW;
+          digitalWrite(data->PIN_Steps, stpState);
+        }
+      }
+    }
+  }
+  if (digitalRead(data->Endstop1) == 1 && digitalRead(data->Endstop2) == 0) {
+    data->Position = 0;
+    return 1;
+  } else if (digitalRead(data->Endstop2) == 1 && digitalRead(data->Endstop1) == 0) {
+    return 2;
+  } else {
+    return 0;
+  }
+}
 bool homing() {
   Serial.println("homed Base with");
   Serial.println(moveAxis(&Base));
@@ -335,7 +403,7 @@ bool homing() {
   Serial.println(moveAxis(&C_Hand));
   return true;
 }
-void SetGoal(MyAxis* data, int goal) {
+void SetGoal(MyAxis * data, int goal) {
   data->goal = goal;
 }
 String SerialData = "";
@@ -356,50 +424,47 @@ void loop() {
       case 237: //L_B
         Serial.println(SerialData);
         SetGoal(&L_Base, SerialData.toInt());
-        moveAxis(&L_Base);
+        moveStepperAxis(&L_Base);
         break;
       case 256: //__B
         Serial.println(SerialData);
-        SetGoal(&Base,SerialData.toInt());
+        SetGoal(&Base, SerialData.toInt());
         moveAxis(&Base);
         break;
       case 273: //__S
         Serial.println(SerialData);
-        SetGoal(&Shoulder,SerialData.toInt());
+        SetGoal(&Shoulder, SerialData.toInt());
         moveAxis(&Shoulder);
         break;
       case 248: //T_E
         Serial.println(SerialData);
-        SetGoal(&T_Ellbow,SerialData.toInt());
+        SetGoal(&T_Ellbow, SerialData.toInt());
         moveAxis(&T_Ellbow);
         break;
       case 230: //B_E
         Serial.println(SerialData);
-        SetGoal(&B_Ellbow,SerialData.toInt());
+        SetGoal(&B_Ellbow, SerialData.toInt());
         moveAxis(&B_Ellbow);
         break;
       case 233: //B_H
         Serial.println(SerialData);
-        SetGoal(&B_Hand,SerialData.toInt());
+        SetGoal(&B_Hand, SerialData.toInt());
         moveAxis(&B_Hand);
         break;
       case 251: //T_H
-        Serial.println(SerialData);
-        SetGoal(&T_Hand,SerialData.toInt());
-        moveAxis(&T_Hand);
+        // Serial.println(SerialData);
+        // SetGoal(&T_Hand, SerialData.toInt());
+        // moveAxis(&T_Hand);
         break;
       case 234: // C_H
         Serial.println(SerialData);
-        SetGoal(&C_Hand,SerialData.toInt());
+        SetGoal(&C_Hand, SerialData.toInt());
         moveAxis(&C_Hand);
         break;
       default:
         Serial.println("Error");
         break;
     }
-
-
-
   }
 
 }
